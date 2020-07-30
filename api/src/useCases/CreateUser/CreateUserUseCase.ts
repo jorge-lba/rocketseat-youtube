a70c5cea95fd73d024cb98475a428350e4e5f2a4
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-07-30 01:41:57 -0300

    Add - Criaçao do UseCase da criaçao de usuario

--- api/src/useCases/CreateUser/CreateUserUseCase.ts
+++ api/src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,19 @@
+import { IUserRepository } from "../../repositories/IUserRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+
 export class CreateUserUseCase {
-    async execute(){
-        
+    constructor(
+       private usersRepository: IUserRepository
+    ){}
+   
+    async execute(data: ICreateUserRequestDTO){
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
+
+        if(userAlreadyExists) throw new Error('User already exists.')
+
+        const user = new User(data)
+
+        await this.usersRepository.save(user)
     }
 }
