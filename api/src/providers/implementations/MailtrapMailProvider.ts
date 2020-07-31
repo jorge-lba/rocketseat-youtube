509b9f66d8ee3ce041f2f9060183e46be560484a
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-07-30 23:42:22 -0300

    Add - Implementaçao da funcionalidade de envio de emails

---
+++ api/src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
+import { IMailProvider, IMessage } from "../IMailProvider";
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail
+    
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth:{
+                user: '6bc2c47437d2e2',
+                pass: 'f4599783c6118f'
+            }
+        })
+    }
+    
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+           to: {
+               name: message.to.name,
+               address: message.to.email
+           },
+           from:{
+               name: message.from.name,
+               address: message.from.email
+           },
+           subject: message.subject,
+           html: message.body
+        })
+    }
+}
