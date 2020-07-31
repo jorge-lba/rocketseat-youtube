279ad29987e60a5a23f46ebd8c0cfb02b36b1e41
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-07-31 00:45:12 -0300

    Add - controller na rota

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,10 @@
 import { Router, request, response } from "express";
+import { createUserController } from "./useCases/CreateUser";
 
 const router = Router()
 
 router.post('/user', (request, response) => {
-    return response.status(201).send()
+    return createUserController.handler(request, response)
 })
 
 export { router }
